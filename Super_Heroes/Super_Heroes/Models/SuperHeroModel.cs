af98dd6d123682375756b400dceb42bed5ce0dd9
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-24 13:16:39 -0500

    was missing some classes from my Model class. had to create new project

---
+++ Super_Heroes/Super_Heroes/Models/SuperHeroModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Super_Heroes.Models
+{
+    public class SuperHeroModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SuperHeroName { get; set; }
+
+        public string primarysuperheroability { get; set; }
+        public string secondarysuperheroability { get; set; }
+        public string alterego { get; set; }
+        public string catchPhrase { get; set; }
+    }
+}
